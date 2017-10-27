1ca28f3332c1c439424d07e9b1a139be11dcc50c
Vatsal <vtslaggarwal@gmail.com>
2017-10-28 04:16:26 +0530

    Major Structural changes made and is made completely ready for work

--- toDoList.js
+++ toDoList.js
@@ -1,20 +1,12 @@
 $(".alignRight").click(function() {
   $("input").slideToggle('fast');
 });
-$("input").focusin(function() {
-  $('input').addClass("inputInFocus");
-});
-$("input").focusout(function() {
-  $('input').removeClass("inputInFocus");
-});
 $('input').keypress(function(event) {
   if (event.which === 13) {
     createNewDiv($('input').val());
     $('input').val("");
   }
 });
-var list = [];
-
 function createNewDiv(text) {
   var image = $("<span class='sideBox'><i class='fa fa-trash' aria-hidden='true'></i></span>");
   var para = $("<span class='task'>" + text + "</span>");
@@ -23,21 +15,13 @@ function createNewDiv(text) {
   $(div).append(image);
   $(div).append(para);
   $("#allToDos").append(div);
-  var oneItem = {
-    completeDiv: div,
-    img: image,
-    par: para
-  };
-  list.push(oneItem);
-  if (list.length % 2 === 1)
-    $(oneItem.completeDiv).addClass("whiteBackground");
-  else {
-    $(oneItem.completeDiv).addClass("greyBackground");
-  }
-  $(div).on("mouseenter", function () {
-    $(image).toggle("slide");
+  $(image).on("click", function () {
+    $(para).removeClass("strikeThrough");
+    $(div).slideToggle('fast', function () {
+      $(this).remove();
+    });
   });
-  $(div).on("mouseleave", function () {
-    $(image).toggle("slide");
+  $(div).on("click", function () {
+    $(this).toggleClass("strikeThrough");
   });
 }
